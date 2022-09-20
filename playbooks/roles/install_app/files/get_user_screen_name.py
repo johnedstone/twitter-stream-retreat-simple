5a3996aba36acbacbb7f491a6c74ea063869a39a
johnedstone <johnedstone@gmail.com>
2022-09-20 14:07:27 -0400

    refactoring for API v2

--- playbooks/roles/install_app/files/get_user_screen_name.py
+++ playbooks/roles/install_app/files/get_user_screen_name.py
@@ -3,13 +3,6 @@ import tweepy, os
 from dotenv import load_dotenv
 load_dotenv()
 
-def get_screen_name_v1(user_id): # API v1.1
-    auth = tweepy.OAuth2BearerHandler(os.getenv('BEARER_TOKEN'))
-    api = tweepy.API(auth)
-    user = api.get_user(user_id=f'{user_id}')
-
-    return user.screen_name
-
 def get_screen_name(user_id): # API v2
     client = tweepy.Client(os.getenv('BEARER_TOKEN'))
     user = client.get_user(id=f'{user_id}')
