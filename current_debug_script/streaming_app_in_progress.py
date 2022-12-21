f612a4c995b5818dcfee0ffb06ce75863e6d51ce
johnedstone <johnedstone@gmail.com>
2022-12-21 13:46:18 -0500

    done: use referenced_tweets to avoid retweeting

--- current_debug_script/streaming_app_in_progress.py
+++ current_debug_script/streaming_app_in_progress.py
@@ -7,36 +7,37 @@ import tweepy
 from dotenv import load_dotenv
 
 load_dotenv()
-print_to_file = os.getenv('PRINT_TO_FILE', 'no') == 'yes'
-logging_file = os.getenv('LOGGING_FILE', 'debug.log')
+LOG_TO_FILE = os.getenv('LOG_TO_FILE', 'no').lower() == 'yes'
+LOGGING_FILE_NAME = os.getenv('LOGGING_FILE_NAME', 'debug.log')
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
 
 
-if print_to_file:
-    logging.basicConfig(level=logging.DEBUG,
-            format='%(asctime)s [%(levelname)s]: %(message)s', filename=logging_file)
+if LOG_TO_FILE:
+    logging.basicConfig(level=LOGLEVEL,
+            format='%(asctime)s [%(levelname)s]: %(message)s', filename=LOGGING_FILE_NAME)
 else:
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=LOGLEVEL,
             format='%(asctime)s [%(levelname)s]: %(message)s')
 
 def create_list(string_list):
     return list(filter(None, [int(ea) for ea in string_list.split(',')]))
 
 def get_stream_rules():
-    stream_rules = [tweepy.StreamRule(value=f'from: {ea}', tag=f'{ea}', id=f'{ea}') for ea in ids_to_follow_list]
+    stream_rules = [tweepy.StreamRule(value=f'from: {ea}', tag=f'{ea}', id=f'{ea}') for ea in IDS_TO_FOLLOW_LIST]
 
     return stream_rules
 
 bearer_token = os.getenv('BEARER_TOKEN')
 
-ids_to_follow = os.getenv('IDS_TO_FOLLOW')
-ids_to_follow_list = create_list(ids_to_follow)
-logging.debug(ids_to_follow_list)
-logging.debug(type(ids_to_follow_list))
+IDS_TO_FOLLOW = os.getenv('IDS_TO_FOLLOW')
+IDS_TO_FOLLOW_LIST = create_list(IDS_TO_FOLLOW)
+logging.debug(IDS_TO_FOLLOW_LIST)
+logging.debug(type(IDS_TO_FOLLOW_LIST))
 
-ids_not_to_publish = os.getenv('IDS_NOT_TO_PUBLISH')
-ids_not_to_publish_list = create_list(ids_to_follow)
-logging.debug(ids_not_to_publish_list)
-logging.debug(type(ids_not_to_publish_list))
+IDS_NOT_PUBLISH = os.getenv('IDS_NOT_TO_PUBLISH')
+IDS_NOT_TO_PUBLISH = create_list(IDS_TO_FOLLOW)
+logging.debug(IDS_NOT_TO_PUBLISH)
+logging.debug(type(IDS_NOT_TO_PUBLISH))
 
 
 class CustomStreamingClient(tweepy.StreamingClient):
@@ -50,23 +51,42 @@ class CustomStreamingClient(tweepy.StreamingClient):
         logging.debug(f'tweet.referenced_tweets: {tweet.referenced_tweets}')
         logging.debug(f'{dir(tweet)}')
 
-        if int(tweet.author_id) in ids_not_to_publish_list:
-            logging.debug('This tweet is in the list not to publish')
+        verified = False
+        publish = False
 
-        if tweet.in_reply_to_user_id:
-            logging.debug('This tweet is a reply')
-        else:
-            logging.debug('This tweet is NOT a reply')
+        while not verified:
+            if tweet.in_reply_to_user_id:
+                logging.debug('This tweet is a reply')
+            else:
+                logging.debug('This tweet is NOT a reply')
 
-        if 'referenced_tweets' in tweet.data.keys():
-            for ea in tweet.data['referenced_tweets']:
-                if ea['type'] == 'quoted':
-                    logging.debug('This is a quote')
-                if ea['type'] == 'retweeted':
-                    logging.debug('This is a retweet')
-        else:
-            logging.debug('This is a simple tweet, i.e. not a Reply, Retweet, nor Comment ("Quoted Tweet")')
-        
+            # For Retweet or Comment (Quote)
+            if 'referenced_tweets' in tweet.data.keys():
+                for ea in tweet.data['referenced_tweets']:
+                    if ea['type'] == 'quoted':
+                        logging.debug('This is a quote')
+                    if ea['type'] == 'retweeted':
+                        logging.debug('This is a retweet')
+
+                    if int(tweet.author_id) not in IDS_NOT_TO_PUBLISH:
+                        if ea.id in IDS_TO_FOLLOW_LIST:
+                            logging.debug('This retreat or quote should already have been "seen"')
+                            verified = True
+                            publish = False
+                            break
+            else:
+                verified = True
+                publish = True
+                logging.debug('This is a simple tweet, i.e. not a Reply, Retweet, nor Comment ("Quoted Tweet")')
+
+            if int(tweet.author_id) in IDS_NOT_TO_PUBLISH:
+                logging.debug('The author_id of this tweet is in the list not to publish')
+                verified = True
+                publish = False
+                break
+
+        logging.debug(f'Verified: {verified}')
+        logging.debug(f'Publish: {publish}')
         logging.debug(f'{"#"*20} END {"#"*20}') 
 
     def on_includes(self, includes):
